bd503a34bd420398ab1e9d71fbe73fa303005e74
hongjun.liao <docici@126.com>
2024-04-21 22:56:23 +0800

    添加--sort_url,支持按URL中的域名频率排序多个候选URL了

--- src/autoiptv.cpp
+++ src/autoiptv.cpp
@@ -41,7 +41,9 @@
 #include "hp/hp_tuple.h"	//hp_tuple
 #include "hp/hp_stdlib.h"	//
 #include "hp/hp_curl.h"	//
+#include "hp/hp_dict.h"	//
 #include "hp/hp_search.h"	//
+#include "libyuarel/yuarel.h"	//yuarel_parse
 #include <getopt.h>		/* getopt_long */
 #include "hp/string_util.h"	//hp_fread
 #include <curl/curl.h>   /* libcurl */
@@ -66,7 +68,7 @@ struct tvg_st{
 	//m3u URL,包含现有的,及从.m3u文件中查找到的
 	//添加'\n'到字串符串结尾以标记为已更新,第1个元素为主URL,如果已更新,则视为该频道已完成更新
 	sds urls[8];
-	int iurls;    //
+	int iurls, rurls;    //
 };
 
 typedef struct m3u_download_ctx {
@@ -77,14 +79,15 @@ typedef struct m3u_download_ctx {
 
 typedef struct m3u_chk_ctx {
 	tvg_st tvgs[20000];
-	int n_tvg, n_chked, n_skip;
+	int n_tvg, n_resp, n_req;
 } m3u_chk_ctx;
 /////////////////////////////////////////////////////////////////////////////////////////
 static 	m3u_download_ctx dctxobj = { 0 }, * g_dctx = &dctxobj;
 static 	m3u_chk_ctx g_chkctxobj = {0}, * g_chkctx = &g_chkctxobj;
 
 /////////////////////////////////////////////////////////////////////////////////////////
-static sds regex_find(char const *buf, char const * pattern, int * so, int * eo){
+static sds regex_find(char const *buf, char const * pattern, int * so, int * eo)
+{
 	assert(buf && pattern);
 	int rc;
 	sds k = 0;
@@ -125,9 +128,39 @@ static sds regex_find(char const *buf, char const * pattern, int * so, int * eo)
 	return k;
 }
 
+hp_tuple2_t(sort_tvgurl_ctx, tvg_st * /*tvg*/, hp_dict_si & /*sidict*/);
+static int sort_tvgurl_cb (const void * a, const void * b, void * arg)
+{
+	int rc;
+	assert(a && b && arg);
+	auto c = (sort_tvgurl_ctx *)arg;
+	auto tvg = c->_1;
+	auto urla = sdsdup(*(sds *)a), urlb = sdsdup(*(sds *)b);
+	auto& sdsdict = c->_2;
+
+	struct yuarel yurla, yurlb;
+	rc = ((yuarel_parse(&yurla, urla) == 0 && yuarel_parse(&yurlb, urlb) == 0));
+	if(!rc) rc = 1;
+	else rc = (sdsdict[yurla.host] != sdsdict[yurlb.host])?
+			sdsdict[yurla.host] > sdsdict[yurlb.host] :
+			((sds *)a - tvg->urls < (sds *)b - tvg->urls);
+
+	sdsfree(urla);sdsfree(urlb);
+	return !rc;
+}
+
+static int sort_tvgurl(tvg_st * tvg, hp_dict_si& sidict)
+{
+	assert(tvg);
+	sort_tvgurl_ctx c = { tvg, sidict };
+	qsort_r(tvg->urls, tvg->rurls, sizeof(sds), sort_tvgurl_cb, &c);
+	return 0;
+}
+
 static int hp_lfind_cb_url(const void * k, const void * e) { return !sdscmp(*(sds *)k, *(sds *)e) == 0; }
 
-static int update_tvgurl_from_m3u(char const * m3u, tvg_st * tvg){
+static int update_tvgurl_from_m3u(char const * m3u, tvg_st * tvg)
+{
 	assert(m3u && tvg);
 	int i, n;
 	char const * k = tvg->k, * end = m3u + strlen(m3u);
@@ -194,19 +227,14 @@ static int on_chk_url(hp_curl * hcurl, CURL *easy_handle, char const * url, sds 
     long res_status;
     curl_easy_getinfo(easy_handle, CURLINFO_RESPONSE_CODE, &res_status);
 
-    //找到该频道第1个尚未被更新的URL,更新它
+	++g_chkctx->n_resp;
+   //找到该频道第1个尚未被更新的URL,更新它
     if(res_status == 200){
     	for(i = 0; tvg->urls[i]; ++i){
     		if(tvg->urls[i][sdslen(tvg->urls[i]) - 1] != '\n'){
     			tvg->urls[i] = sdscatfmt(sdsempty(), "%s\n", url);
-    			if(i == 0){
-					++g_chkctx->n_chked;
-					if(hp_log_level > 0){
-						hp_log(std::cout, "%s: [%d/%d] URL added for tvg='%s', url='%s'\n", __FUNCTION__
-								, g_chkctx->n_chked, g_chkctx->n_tvg, tvg->k, url);
-					}
-    			}
-    			break;
+    			++tvg->rurls;
+     			break;
     		}
     	}
     }
@@ -221,11 +249,12 @@ int autoiptv_main(int argc, char **argv)
 #ifndef NDEBUG
 //	curl_https_main();
 //	rc = test_hp_curl_main(argc, argv); assert(rc == 0);
+	rc = test_hp_dict_main(argc, argv); assert(rc == 0);
 	if(hp_log_level == 0) hp_log_level = 2;
 	{ assert(hp_isblank("")); assert(hp_isblank("\n")); assert(hp_isblank(" "));assert(hp_isblank(" \n"));}
 	{ sds k = regex_find("#EXTINF:https://xxx,CCTV-1 综合\n", ",\\s*\(\\S+\)\\s+", 0, 0); assert(k && strcmp(k, "CCTV-1") == 0); sdsfree(k); }
 	{ sds k = regex_find("#EXTINF:https://xxx,湖南卫视 \n", ",\\s*\(\\S+\)\\s+", 0, 0); assert(k && strcmp(k, "湖南卫视") == 0); sdsfree(k); }
-	{ sds k = regex_find("#EXTINF:https://xxx,湖南卫视 \n", ",\\s*\([\u4e00-\u9fa5]+\)\\s+", 0, 0); assert(k && strcmp(k, u8"湖南卫视") == 0); sdsfree(k); }
+	{ sds k = regex_find("#EXTINF:https://xxx,湖南卫视 \n", ",\\s*\([\u4e00-\u9fa5]+\)\\s+", 0, 0); assert(k && strcmp(k, "湖南卫视") == 0); sdsfree(k); }
 
 	{ 	sds buf = hp_fread("cctv.m3u"); tvg_st tvgobj = { .k = sdsnew("CCTV-1") }, * tvg = &tvgobj;
 		rc = update_tvgurl_from_m3u(buf, &tvgobj);assert(rc > 0);
@@ -236,22 +265,46 @@ int autoiptv_main(int argc, char **argv)
 	{ 	sds buf = hp_fread("cctv.m3u"); tvg_st tvgobj = { .k = sdsnew("不存在卫视") }, * tvg = &tvgobj;
 		rc = update_tvgurl_from_m3u(buf, &tvgobj);assert(rc == 0);
 		sdsfree(buf);sdsfree(tvg->k); }
-
+	//选中域名频率高的那个
+	{ m3u_chk_ctx chkctxobj = {0}, * chkctx = &chkctxobj; hp_dict_si sidict;
+	tvg_st tvgobj = { .urls = {sdsnew("https://node1.olelive.com:6443/live/CCTV1HD/hls.m3u8\n"),
+	sdsnew("http://ottrrs.hl.chinamobile.com/PLTV/88888888/224/3221226016/index.m3u8\n")}, .rurls = 2}, * tvg = &tvgobj;;
+	sidict["ottrrs.hl.chinamobile.com"] = 1;sidict["node1.olelive.com"] = 2;
+	sort_tvgurl(tvg, sidict);
+	assert(strcmp(tvg->urls[0], "https://node1.olelive.com:6443/live/CCTV1HD/hls.m3u8\n") == 0);
+	for(i = 0; tvg->urls[i]; ++i) sdsfree(tvg->urls[i]);}
+	{ m3u_chk_ctx chkctxobj = {0}, * chkctx = &chkctxobj; hp_dict_si sidict;
+	 tvg_st tvgobj = { .urls = {sdsnew("http://ottrrs.hl.chinamobile.com/PLTV/88888888/224/3221226016/index.m3u8\n"),
+	sdsnew("https://node1.olelive.com:6443/live/CCTV1HD/hls.m3u8\n")}, .rurls = 2}, * tvg = &tvgobj;;
+	sidict["ottrrs.hl.chinamobile.com"] = 1;sidict["node1.olelive.com"] = 2;
+	sort_tvgurl(tvg, sidict);
+	assert(strcmp(tvg->urls[0], "https://node1.olelive.com:6443/live/CCTV1HD/hls.m3u8\n") == 0);
+	for(i = 0; tvg->urls[i]; ++i) sdsfree(tvg->urls[i]);}
+	//如果域名频率相同，选中最近更新的那个
+	{ m3u_chk_ctx chkctxobj = {0}, * chkctx = &chkctxobj; hp_dict_si sidict;
+	 tvg_st tvgobj = { .urls = {sdsnew("https://node1.olelive.com:6443/live/CCTV1HD/hls.m3u8\n"),
+	sdsnew("http://ottrrs.hl.chinamobile.com/PLTV/88888888/224/3221226016/index.m3u8\n")}, .rurls = 2}, * tvg = &tvgobj;;
+	sidict["ottrrs.hl.chinamobile.com"] = 2;sidict["node1.olelive.com"] = 2;
+	sort_tvgurl(tvg, sidict);
+	assert(strcmp(tvg->urls[0], "https://node1.olelive.com:6443/live/CCTV1HD/hls.m3u8\n") == 0);
+	for(i = 0; tvg->urls[i]; ++i) sdsfree(tvg->urls[i]);}
 #endif
 	//--m3u, --url
 	sds opt_m3ufile = sdsempty(), opt_urlfile = sdsempty();
 	int opt_multi_url = 0;
 	int opt_timeout = 60;
+	int opt_sort_url = 0;
 
     /* parse argc/argv */
 	int opt;
+	struct option long_options[] = {
+			{ "m3u\0--m3u=<FILE>.m3u  template file", required_argument, 0, 0 }
+		,	{ "url\0--url=<FILE>      text file, one URL per line", required_argument, 0, 0 }
+		,	{ "timeout\0--timeout=<INT>   timeout in seconds", required_argument, 0, 0 }
+		,	{ "sort_url\0--sort_url=<0|1>  指定如何挑选频道的URL, 0:按URL的域名频率排序;1:按请求最快回应排序", required_argument, 0, 0 }
+		, 	{ 0, 0, 0, 0 } };
 	while (1) {
 		int option_index = 0;
-		static struct option long_options[] = {
-				{ "m3u", required_argument, 0, 0 }
-			,	{ "url", required_argument, 0, 0 }
-			,	{ "timeout", required_argument, 0, 0 }
-			, 	{ 0, 0, 0, 0 } };
 
 		opt = getopt_long(argc, argv, "mv:Vh", long_options, &option_index);
 		if (opt == -1)
@@ -262,6 +315,7 @@ int autoiptv_main(int argc, char **argv)
 			if     (option_index == 0) opt_m3ufile = sdscpy(opt_m3ufile, arg);
 			else if(option_index == 1) opt_urlfile = sdscpy(opt_urlfile, arg);
 			else if(option_index == 2) { opt_timeout = atoi(arg); if(opt_timeout == 0) opt_timeout = 60; }
+			else if(option_index == 3) { opt_sort_url = atoi(arg); }
 			break;
 		}
 		case 'V':
@@ -280,8 +334,12 @@ int autoiptv_main(int argc, char **argv)
 	}
 	if(!(rc == 0 && sdslen(opt_m3ufile) > 0)) {
 		hp_log(std::cout, "autoiptv - auto update an IPTV .m3u file's URLs\n"
-				"Usage: %s autoiptv --m3u cctv.m3u --url url.txt --timeout 60\n"
+				"Usage: %s autoiptv --m3u cctv.m3u --url url.txt --timeout 60 --sort_url 0\nOptions:\n"
 				, argv[0]);
+		for(i = 0; long_options[i].name; ++i){
+			char const * p = strchr(long_options[i].name, '\0');
+			fprintf(stdout, "  %s\n", (p? p + 1 : long_options[i].name));
+		}
 		return 0;
 	}
 
@@ -357,7 +415,7 @@ int autoiptv_main(int argc, char **argv)
 			}
 		}
 		 //检测该频道的所有URL
-		if(tvg->urls[0]){ //至少有一个用来检测的URL
+		if(tvg->iurls > 0){ //至少有一个用来检测的URL
 			if(hp_log_level > 0)
 				hp_log(std::cout, "%s: checking tvg='%s' ...\n", __FUNCTION__, tvg->k);
 			for(j = 0; j < tvg->iurls; ++j){
@@ -365,39 +423,62 @@ int autoiptv_main(int argc, char **argv)
 					std::cout << "\t" << tvg->urls[j] << std::endl;
 				rc = hp_curladd(hcurl, curl_easy_init(), tvg->urls[j], 0/*curl_slist * hdrs*/, 0/*form*/, 0/*char const * resp*/
 						, 0/*on_progress*/, on_chk_url, tvg/*void * arg*/, 0/*void * flags*/);
+				if(rc == 0) ++g_chkctx->n_req;
 			}
 		}
 		else  {
 			hp_log(std::cout, "%s: NO URL for tvg='%s', skipped\n", __FUNCTION__, tvg->k);
-			tvg->urls[0] = sdsnew("\n");
-			++g_chkctx->n_skip;
 		}
 
 		++g_chkctx->n_tvg;
 	}
 	//run loop
 	time_t st =  time(0);
-	int n_chked;
 	do{
 #if (defined HAVE_SYS_TIMERFD_H) && (defined HAVE_SYS_EPOLL_H)
 		hp_epoll_run(loop, 1);
 #else
 		uv_run(loop, UV_RUN_ONCE);
 #endif
-		//查检下是否所有URL都已检测更新完毕
-		n_chked = 0;
+	}while(g_chkctx->n_resp < g_chkctx->n_req && difftime(time(0), st) < opt_timeout);
+
+	hp_log(std::cout, "%s: done! updated total tvgs=%d, URL response/request=%d/%d, %d%% replied\n", __FUNCTION__
+			, g_chkctx->n_tvg, g_chkctx->n_resp, g_chkctx->n_req
+			, g_chkctx->n_resp == g_chkctx->n_req?100 : (int)((g_chkctx->n_resp)  / ( g_chkctx->n_req * 1.001) * 100));
+
+	/*
+	 * 频道URL挑选逻辑：
+	 * 通过将URL中的域名分组得到频率,作为指标进行URL排序,将该指标最高的URL作为频道的URL,如果频率相同,则取更快响应的那个作为频道的URL
+	 * 背后的想法是：如果某个域名多次出现，则可认为域名对应的服务端更稳定，从而URL的有效性更长
+	 */
+	hp_dict_si sidict;
+	if(opt_sort_url == 0){
+		//统计域名频率
 		for(i = 0; i < g_chkctx->n_tvg; ++i){
-			sds murl = g_chkctx->tvgs[i].urls[0];
-			if(murl && murl[sdslen(murl) - 1] == '\n') { //有的频道可能并无URL(或已注释)
-				++n_chked;
+			tvg_st * tvg = g_chkctx->tvgs + i;
+			if(!tvg->urls[0]) continue;
+			for(j = 0; j < tvg->rurls; ++j){
+				sds url = sdsdup(tvg->urls[j]);
+				struct yuarel yurl;
+				if(yuarel_parse(&yurl, url) == 0){
+					++sidict[yurl.host];
+				}
+				sdsfree(url);
 			}
 		}
-	}while(n_chked != g_chkctx->n_tvg && difftime(time(0), st) < opt_timeout);
-
-	hp_log(std::cout, "%s: done! tvg checked/skipped/total=%d/%d/%d, %d%% updated\n", __FUNCTION__
-			, g_chkctx->n_chked, g_chkctx->n_skip, g_chkctx->n_tvg
-			, g_chkctx->n_chked + g_chkctx->n_skip == g_chkctx->n_tvg?
-					100 : (int)((g_chkctx->n_chked + g_chkctx->n_skip)  / ( g_chkctx->n_tvg * 1.001) * 100));
+		//开始排序
+		for(i = 0; i < g_chkctx->n_tvg; ++i){
+			tvg_st * tvg = g_chkctx->tvgs + i;
+			if(tvg->rurls >= 2) sort_tvgurl(tvg, sidict);
+			if(hp_log_level > 0){
+				hp_log(std::cout, "%s: Chosen URL from total=%d for tvg='%s'%s\n", __FUNCTION__
+						, tvg->rurls, tvg->k, (tvg->rurls > 0? ":" : ", no available URL, skipped"));
+				for(j = 0; j < tvg->rurls; ++j){
+					std::cout << "\t" << tvg->urls[j];
+				}
+			}
+		}
+	}
 	//写回--m3u文件
 	FILE * f = fopen(opt_m3ufile, "w");
 	if(f){
@@ -418,6 +499,7 @@ int autoiptv_main(int argc, char **argv)
 			}
 		}
 		fclose(f);
+		hp_log(std::cout, "%s: tempalte file '%s' updated\n", __FUNCTION__, opt_m3ufile);
 	}
 	//clean
 	sdsfreesplitres(urls, n_urls);
@@ -437,7 +519,7 @@ int autoiptv_main(int argc, char **argv)
 		if(tvg->desc) sdsfree(tvg->desc);
 		sdsfree(tvg->extinf);
 		sdsfree(tvg->k);
-		for(j = 0; tvg->urls[j]; ++j)
+		for(j = 0; j < tvg->iurls; ++j)
 			sdsfree(tvg->urls[j]);
 	}
 	return 0;
